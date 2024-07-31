3d0198a4f7dcbe43e7a1fa8fb0697f310742347b
alan <alanaudia93@gmail.com>
2024-07-31 18:34:48 -0400

    Added PORT to app.py

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def classify_text():
         return jsonify({'error': 'Error in model inference'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
